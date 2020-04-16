983044f5cc49d8c27d6ac084b4c1717c66c88a5e
Joachim <joachim.strudsholm@solutionteam.dk>
2020-04-16 04:14:07 +0200

    Files renamed

---
+++ BachelorsProjectWebApp/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BachelorsProjectWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FindOffice(string Search)
+        {
+            ViewBag.searchid = Search;
+            return View("FindOffice");
+        }
+
+        public ActionResult Papers( string id)
+        {
+            ViewBag.papers = id;
+            return View();
+        }
+
+
+        public ActionResult Navigation()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
